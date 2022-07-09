845802834575a2559d3cc1ff1b075e81c2aa75b8
Mercy-Jepchirchir <mjepchirchirkemboi@gmail.com>
2022-07-09 09:15:42 +0300

    my-contact recycler view

---
+++ app/src/main/java/dev/chirii/mycontacts/contact.kt
@@ -0,0 +1,9 @@
+package dev.chirii.mycontacts
+
+data class contact(
+    var name : String,
+    var phoneNumber : String,
+    var email : String,
+    var address: String,
+    var image : String
+)
